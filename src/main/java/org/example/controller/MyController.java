c42c061ea092fe3f43ba672a9eaa819a5394c1fc
jana <jantschick3@gmail.com>
2023-02-09 16:02:24 +0300

    first

--- src/main/java/org/example/controller/MyController.java
+++ src/main/java/org/example/controller/MyController.java
@@ -1,2 +1,14 @@
-package org.example.controller;public class MyController {
+package org.example.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MyController {
+
+    @RequestMapping("/")
+    public String showFirstView() {
+        return "first-view";
+    }
+
 }
---
+++ src/main/webapp/WEB-INF/view/first-view.jsp
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h2>I wish you luck in Strong MVC learning</h2>
+
+</body>
+</html>
